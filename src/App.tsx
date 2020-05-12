241d345acaecc0fc4331d869ebc7a8d9bb1d14b4
adashuai5 <ad873606339@gmail.com>
2020-05-13 00:27:10 +0800

    基本实现 dock 效果

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from "react";
-import Dock from "./views/Dock";
+import React, { useState, useEffect } from "react";
+import { dockEffect } from "./effect";
 import "./styles/App.scss";
 
 function App() {
+  const list = [
+    "Launchpad.png",
+    "Finder.png",
+    "PrefApp.png",
+    "Terminal.png",
+    "Calculator.png",
+  ];
+  const [dockList] = useState(list);
+
+  useEffect(() => {
+    dockEffect({ el: "AppFooter" });
+  }, []);
   return (
     <div className="App">
       <header className="AppFinder"></header>
+      <div className="DockBackground"></div>
       <footer className="AppFooter">
-        <Dock />
+        {dockList.map((item, index) => {
+          return (
+            <img
+              src={require("./assets/image/" + item)}
+              alt={item}
+              key={index}
+            />
+          );
+        })}
       </footer>
     </div>
   );
---
+++ src/effect.js
@@ -0,0 +1,61 @@
+export function dockEffect(config) {
+  /**
+   * config
+   * el: 对那个父元素里面的元素显示dock效果，ClassName(string)
+   * toTag: img(默认是查看对应元素的img)
+   * cb: 回调函数(有监听事件就应该给回调函数)
+   */
+  const dockWrap = document.getElementsByClassName(config.el)[0];
+  const toTag = config.toTag || "img";
+  const img = dockWrap.getElementsByTagName(toTag);
+  const imgAmount = img.length;
+  let imgScale = 0;
+  let x = 0,
+    y = 0,
+    i = 0
+  function initPage() {
+    for (i = 0; i < imgAmount; i++) {
+      img[i].width = 76;
+    }
+  }
+
+  // 获取相对于HTML的y轴
+  function getOffsetTop(el) {
+    if (el.offsetParent == null) {
+      return el.offsetTop;
+    }
+
+    return el.offsetTop + getOffsetTop(el.offsetParent);
+  }
+
+  function initEvent() {
+    dockWrap.onmousemove = function (e) {
+      e = e || window.event;
+      for (i = 0; i < imgAmount; i++) {
+        x = e.clientX - (img[i].offsetLeft + 76);
+        y =
+          img[i].offsetTop +
+          getOffsetTop(dockWrap) +
+          img[i].offsetHeight / 2 -
+          e.clientY;
+        imgScale = 1 - Math.sqrt(x * x + y * y) / 300;
+        if (imgScale < 0.5) {
+          imgScale = 0.5;
+        }
+        img[i].width = 152 * imgScale;
+      }
+      config.cb && config.cb()
+    };
+    dockWrap.onmouseleave = () => {
+      for (i = 0; i < imgAmount; i++) {
+        img[i].width = 152 * 0.5
+      }
+    }
+  }
+  function init() {
+    initPage();
+    initEvent();
+  }
+
+  init();
+}
