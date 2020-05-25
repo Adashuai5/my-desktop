062b2b37900b6fb651666caac096b102ab2a9fe9
adashuai5 <ad873606339@gmail.com>
2020-05-26 01:53:38 +0800

    canvas save

--- src/components/Drawing/index.tsx
+++ src/components/Drawing/index.tsx
@@ -8,7 +8,91 @@ import "./index.scss";
 export const Drawing = React.memo(() => {
   const { show, hide, RenderModal } = useModal();
   const [isDrawingShow, setDrawingShow] = useContext(FooterContext);
+  const [using, setUsing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(5);
+  const [step, setStep] = useState(-1);
+  const [canvasHistory, setCanvasHistory] = useState([]);
+  const [lastPoint, setLastPoint] = useState({
+    x: undefined,
+    y: undefined,
+  });
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+  const drawLine = useCallback(
+    (x1, y1, x2, y2) => {
+      console.log(canvas, "canvas");
+      if (canvas && canvas instanceof HTMLCanvasElement) {
+        const context: any = canvas.getContext("2d");
+        context.fillStyle = "black";
+        context.strokeStyle = "black";
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.lineWidth = lineWidth;
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+      }
+    },
+    [canvas, lineWidth]
+  );
+  const drawCircle = useCallback(
+    (x, y, radius) => {
+      if (canvas && canvas instanceof HTMLCanvasElement) {
+        const context: any = canvas.getContext("2d");
+        context.fillStyle = "black";
+        context.strokeStyle = "black";
+        context.beginPath();
+        context.arc(x, y, radius, 0, Math.PI * 2);
+        context.fill();
+      }
+    },
+    [canvas]
+  );
+  // const saveFragment = useCallback(() => {
+  //   setStep((step) => step++);
+  //   if (step < canvasHistory.length) {
+  //     canvasHistory.length = step;
+  //   }
+  //   canvasHistory.push(canvas.toDataURL());
+  //   // back.classList.add("active");
+  //   // go.classList.remove("active");
+  // }, [context]);
+
+  const handleMouseDown = useCallback(({ clientX, clientY }) => {
+    setUsing(true);
+    setLastPoint({
+      x: clientX,
+      y: clientY,
+    });
+  }, []);
+
+  const handleMouseMove = useCallback(
+    ({ clientX, clientY, target }) => {
+      if (!canvas || target !== canvas || !using || !isDrawingShow) return;
+      const newPoint = {
+        x: clientX,
+        y: clientY,
+      };
+      drawCircle(clientX, clientY, lineWidth / 2);
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+    },
+    [isDrawingShow, using, canvas, lineWidth, lastPoint, drawLine, drawCircle]
+  );
+  const handleMouseUp = useCallback(() => {
+    setUsing(false);
+    // saveFragment();
+  }, []);
+
   useEffect(isDrawingShow ? show : hide, [isDrawingShow]);
+  useEffect(() => {
+    if (using) {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    }
+  }, [using, handleMouseMove, handleMouseUp]);
   return (
     <RenderModal
       data={{
@@ -33,7 +117,7 @@ export const Drawing = React.memo(() => {
           }}
           onMaximizeClick={show}
         ></TitleBar>
-        <div className="drawing"></div>
+        <canvas id="canvas" onMouseDown={handleMouseDown}></canvas>
       </div>
     </RenderModal>
   );
