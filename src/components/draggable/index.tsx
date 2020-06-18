7856ac607f60ed6f20dbb129e27ace33ff8e3293
yd.zhou <yd.zhou@foxmail.com>
2020-06-18 16:19:04 +0800

    move draggable from Model

---
+++ src/components/draggable/index.tsx
@@ -0,0 +1,137 @@
+import React, {
+  useState,
+  useCallback,
+  useMemo,
+  useEffect,
+  CSSProperties,
+} from "react";
+
+type Props = {
+  children: React.ReactChild;
+  domEl: HTMLDivElement;
+  onDrag: (T: any) => void;
+  onDragEnd: () => void;
+  data: {
+    width: number;
+    height: number;
+    id: string;
+    moveId: string;
+    isShow: boolean;
+  };
+};
+const Draggable = ({ children, domEl, data, onDrag, onDragEnd }: Props) => {
+  const dragEl = document.getElementById(data.id) as HTMLDivElement;
+  const moveEl = document.getElementById(data.moveId) as HTMLDivElement;
+  const localPosition = localStorage.getItem(data.id) || null;
+  const initPosition = localPosition
+    ? JSON.parse(localPosition)
+    : {
+        x: data.width === -1 ? 0 : (window.innerWidth - data.width) / 2,
+        y: data.height === -1 ? 0 : (window.innerHeight - data.height) / 2,
+      };
+  const [state, setState] = useState({
+    isDragging: false,
+    origin: { x: 0, y: 0 },
+    position: initPosition,
+  });
+
+  const handleMouseDown = useCallback(({ clientX, clientY }) => {
+    setState((state) => ({
+      ...state,
+      isDragging: true,
+      origin: {
+        x: clientX - state.position.x,
+        y: clientY - state.position.y,
+      },
+    }));
+  }, []);
+
+  const handleMouseMove = useCallback(
+    ({ clientX, clientY, target }) => {
+      if (!state.isDragging || (moveEl && target !== moveEl)) return;
+      let x = clientX - state.origin.x;
+      let y = clientY - state.origin.y;
+      if (x <= 0) {
+        x = 0;
+      } else if (x > window.innerWidth - dragEl.offsetWidth) {
+        x = window.innerWidth - dragEl.offsetWidth;
+      }
+      if (y <= 0) {
+        y = 0;
+      } else if (y > window.innerHeight - dragEl.offsetHeight) {
+        y = window.innerHeight - dragEl.offsetHeight;
+      }
+      const newPosition = { x, y };
+      setState((state) => ({
+        ...state,
+        position: newPosition,
+      }));
+      onDrag({ newPosition, dragEl });
+    },
+    [state.isDragging, state.origin, moveEl, dragEl, onDrag]
+  );
+
+  const handleMouseUp = useCallback(() => {
+    if (state.isDragging) {
+      setState((state) => ({
+        ...state,
+        isDragging: false,
+      }));
+
+      onDragEnd();
+    }
+  }, [state.isDragging, onDragEnd]);
+
+  useEffect(() => {
+    if (data.width === -1) {
+      setState({
+        isDragging: false,
+        origin: { x: 0, y: 0 },
+        position: { x: 0, y: 0 },
+      });
+    }
+  }, [data.width]);
+
+  useEffect(() => {
+    if (!domEl) return;
+    domEl.addEventListener("mousemove", handleMouseMove);
+    domEl.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      domEl.removeEventListener("mousemove", handleMouseMove);
+      domEl.removeEventListener("mouseup", handleMouseUp);
+      if (data.width !== -1) {
+        localStorage.setItem(data.id, JSON.stringify(state.position));
+      }
+    };
+  }, [
+    domEl,
+    handleMouseMove,
+    handleMouseUp,
+    data.id,
+    data.width,
+    state.position,
+  ]);
+
+  const styles = useMemo(
+    () => ({
+      left: `${state.position.x}px`,
+      top: `${state.position.y}px`,
+      zIndex: state.isDragging ? 2 : 1,
+      display: data.isShow ? "block" : "none",
+      position: "absolute",
+    }),
+    [state.isDragging, state.position, data.isShow]
+  );
+
+  return (
+    <div
+      id={data.id}
+      style={styles as CSSProperties}
+      onMouseDown={handleMouseDown}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Draggable;
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,15 +1,10 @@
 import ReactDOM from "react-dom";
-import React, {
-  useState,
-  useCallback,
-  useMemo,
-  useEffect,
-  CSSProperties,
-} from "react";
+import React from "react";
+import Draggable from "../draggable/index";
 
 type Props = {
   children: React.ReactChild;
-  closeModal: () => void;
+  closeModal?: () => void;
   onDrag: (T: any) => void;
   onDragEnd: () => void;
   data: {
@@ -21,124 +16,16 @@ type Props = {
   };
 };
 
-const Modal = React.memo(
-  ({ children, closeModal, onDrag, onDragEnd, data }: Props) => {
-    const domEl = document.getElementById("main-view") as HTMLDivElement;
-    if (!domEl) return null;
-    const dragEl = document.getElementById(data.id) as HTMLDivElement;
-    const moveEl = document.getElementById(data.moveId) as HTMLDivElement;
-    const localPosition = localStorage.getItem(data.id) || null;
-    const initPosition = localPosition
-      ? JSON.parse(localPosition)
-      : {
-          x: data.width === -1 ? 0 : (window.innerWidth - data.width) / 2,
-          y: data.height === -1 ? 0 : (window.innerHeight - data.height) / 2,
-        };
-    const [state, setState] = useState({
-      isDragging: false,
-      origin: { x: 0, y: 0 },
-      position: initPosition,
-    });
+const Modal = React.memo(({ children, onDrag, onDragEnd, data }: Props) => {
+  const domEl = document.getElementById("main-view") as HTMLDivElement;
+  if (!domEl) return null;
 
-    const handleMouseDown = useCallback(({ clientX, clientY }) => {
-      setState((state) => ({
-        ...state,
-        isDragging: true,
-        origin: {
-          x: clientX - state.position.x,
-          y: clientY - state.position.y,
-        },
-      }));
-    }, []);
-
-    const handleMouseMove = useCallback(
-      ({ clientX, clientY, target }) => {
-        if (!state.isDragging || (moveEl && target !== moveEl)) return;
-        let x = clientX - state.origin.x;
-        let y = clientY - state.origin.y;
-        if (x <= 0) {
-          x = 0;
-        } else if (x > window.innerWidth - dragEl.offsetWidth) {
-          x = window.innerWidth - dragEl.offsetWidth;
-        }
-        if (y <= 0) {
-          y = 0;
-        } else if (y > window.innerHeight - dragEl.offsetHeight) {
-          y = window.innerHeight - dragEl.offsetHeight;
-        }
-        const newPosition = { x, y };
-        setState((state) => ({
-          ...state,
-          position: newPosition,
-        }));
-        onDrag({ newPosition, domEl });
-      },
-      [state.isDragging, state.origin, moveEl, dragEl, onDrag, domEl]
-    );
-
-    const handleMouseUp = useCallback(() => {
-      if (state.isDragging) {
-        setState((state) => ({
-          ...state,
-          isDragging: false,
-        }));
-
-        onDragEnd();
-      }
-    }, [state.isDragging, onDragEnd]);
-
-    useEffect(() => {
-      if (data.width === -1) {
-        setState({
-          isDragging: false,
-          origin: { x: 0, y: 0 },
-          position: { x: 0, y: 0 },
-        });
-      }
-    }, [data.width]);
-
-    useEffect(() => {
-      if (!domEl) return;
-      domEl.addEventListener("mousemove", handleMouseMove);
-      domEl.addEventListener("mouseup", handleMouseUp);
-      return () => {
-        domEl.removeEventListener("mousemove", handleMouseMove);
-        domEl.removeEventListener("mouseup", handleMouseUp);
-        if (data.width !== -1) {
-          localStorage.setItem(data.id, JSON.stringify(state.position));
-        }
-      };
-    }, [
-      domEl,
-      handleMouseMove,
-      handleMouseUp,
-      data.id,
-      data.width,
-      state.position,
-    ]);
-
-    const styles = useMemo(
-      () => ({
-        left: `${state.position.x}px`,
-        top: `${state.position.y}px`,
-        zIndex: state.isDragging ? 2 : 1,
-        display: data.isShow ? "block" : "none",
-        position: "absolute",
-      }),
-      [state.isDragging, state.position, data.isShow]
-    );
-
-    return ReactDOM.createPortal(
-      <div
-        style={styles as CSSProperties}
-        onMouseDown={handleMouseDown}
-        id={data.id}
-      >
-        {children}
-      </div>,
-      domEl
-    );
-  }
-);
+  return ReactDOM.createPortal(
+    <Draggable domEl={domEl} onDrag={onDrag} onDragEnd={onDragEnd} data={data}>
+      {children}
+    </Draggable>,
+    domEl
+  );
+});
 
 export default Modal;
