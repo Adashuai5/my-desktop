2177732fd6034be959482843849e4593e6f61b4a
adashuai5 <ad873606339@gmail.com>
2020-07-05 22:09:31 +0800

    init main

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 import "./styles/App.scss";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <div id="main-view"/>
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+
+const Main = () => {
+
+  return (
+    <div className='AppMain'>
+      <div id="main-view"/>
+    </div>
+  )
+}
+
+export default Main
